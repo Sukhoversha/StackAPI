1fd932ed295cda209267e6397942422722d89fa9
Andriy Sukhoversha <48355842+Sukhoversha@users.noreply.github.com>
2020-06-19 13:53:22 +0200

    Updated supported tags

--- Krakow_Team_Stack_answers.py
+++ Krakow_Team_Stack_answers.py
@@ -4,29 +4,43 @@ import gspread
 import sys
 
 today = datetime.now()
+print(today)
 start = today - timedelta(days=today.weekday()+1)
 end = start + timedelta(days=7)
 fromdate = datetime.timestamp(start)
-todate = datetime.timestamp(end)
+todate = datetime.timestamp(today)
 cnt_k8s = 0
 cnt_infra = 0
 cnt_platform = 0
 cnt_bigdata = 0
 question_ids = []
+answers_ids = []
 valid_q = []
 row = 2
 week = start.strftime("%Y-%m-%d")
 
 if len(sys.argv) < 2:
     print('Start date is not set. Default is start of the week: '+ week)
-else:
+elif len(sys.argv) == 2:
     try:
-        var_date = datetime.strptime(sys.argv[1], '%Y-%m-%d')
-        start = var_date
-        fromdate = datetime.timestamp(var_date)
+        start = datetime.strptime(sys.argv[1], '%Y-%m-%d')
+        # start = var_date
+        fromdate = datetime.timestamp(start)
         week = 'Custom date'
     except ValueError:
         raise ValueError("Incorrect data format, should be YYYY-MM-DD")
+elif len(sys.argv) == 3:
+    try:
+        start = datetime.strptime(sys.argv[1], '%Y-%m-%d')
+        end = datetime.strptime(sys.argv[2], '%Y-%m-%d')
+        fromdate = datetime.timestamp(start)
+        todate = datetime.timestamp(end)
+        week = 'Custom date'
+    except ValueError:
+        raise ValueError("Incorrect data format, should be YYYY-MM-DD")
+else:
+    print ('Too many parameters. Should be maximum 2: from_date, to_date')
+    exit()
 
 # Initialize Google spreadsheet with service account.
 # Credentails are stored in ~/.config/gspread/service_account.json
@@ -51,6 +65,7 @@ wks.update_cell(1, 2, 'User')
 wks.update_cell(1, 3, 'Shard')
 wks.update_cell(1, 4, 'Is Accepted')
 wks.update_cell(1, 5, 'Score')
+wks.update_cell(1, 6, 'Creation Date')
 wks.update_cell(1, 7, 'Last update')
 wks.update_cell(1, 8, 'Script is running...')
 wks.format("G1:H1", {
@@ -76,16 +91,16 @@ sf_infra_users = [527253, 551540, 545593, 504596]
 sf_bigdata_users = [529478, 475392]
 sf_platform_users = []
 
-tags = ['kubernetes', 'minikube', 'istio', 'gcp', 'k8s', 'gke', 'google-kubernetes-engine', 'google-cloud-firestore', 'google-cloud-platform', 'google-compute-engine', 'google-cloud-composer', 'google-data-studio', 'google-bigquery', 'google-app-engine', 'firebase', 'kubectl', 'airflow']
+tags = ['bigtable', 'editor-addons', 'gae-datastore', 'gce-persistent-disk', 'gcloud', 'gcloud-intellij', 'gcloud-java', 'gcloud-node', 'gcloud-python', 'gcsfuse', 'gcutil', 'gke-networking', 'gmail-addons', 'gmail-api', 'gmail-imap', 'gmail-promo-tab', 'google-admin-sdk', 'google-anthos', 'google-app-engine', 'google-apps-activity', 'google-apps-marketplace', 'google-apps-script', 'google-bigquery', 'google-calendar-api', 'google-cdn', 'google-chat', 'google-classroom', 'google-cloud-armor', 'google-cloud-bigtable', 'google-cloud-build', 'google-cloud-cdn', 'google-cloud-composer', 'google-cloud-data-fusion', 'google-cloud-data-transfer', 'google-cloud-dataflow', 'google-cloud-datastore', 'google-cloud-debugger', 'google-cloud-dlp', 'google-cloud-dns', 'google-cloud-dotnet', 'google-cloud-eclipse', 'google-cloud-endpoints', 'google-cloud-filestore', 'google-cloud-firestore', 'google-cloud-functions', 'google-cloud-http-load-balancer', 'google-cloud-intellij', 'google-cloud-interconnect', 'google-cloud-internal-load-balancer', 'google-cloud-kms', 'google-cloud-logging', 'google-cloud-memorystore', 'google-cloud-monitoring', 'google-cloud-network-load-balancer', 'google-cloud-networking', 'google-cloud-platform', 'google-cloud-powershell', 'google-cloud-python', 'google-cloud-router', 'google-cloud-run', 'google-cloud-scheduler', 'google-cloud-sdk', 'google-cloud-source-repos', 'google-cloud-spanner', 'google-cloud-sql', 'google-cloud-storage', 'google-cloud-talent-solution', 'google-cloud-tasks', 'google-cloud-tcp-proxy', 'google-cloud-trace', 'google-cloud-visualstudio', 'google-cloud-vpn', 'google-compute-engine', 'google-container-builder', 'google-container-os', 'google-container-registry', 'google-deployment-manager', 'google-docs', 'google-docs-api', 'google-drive-android-api', 'google-drive-api', 'google-drive-picker', 'google-drive-realtime-api', 'google-drive-sdk', 'google-eclipse-plugin', 'google-genomics', 'google-kubernetes-engine', 'google-people', 'google-people-api', 'google-persistent-disk', 'google-picker', 'google-plugin-eclipse', 'google-sheets', 'google-sheets-api', 'google-slides', 'google-slides-api', 'google-stackdriver', 'google-tasks', 'google-tasks-api', 'google-vault-api', 'google-voice', 'gsuite-addons', 'gsutil', 'hangout', 'hangouts-api', 'hangouts-chat', 'istio', 'kube-dns', 'kube-proxy', 'kubectl', 'kubelet', 'kubernetes', 'kubernetes-go-client', 'kubernetes-health-check', 'kubernetes-ingress', 'maven-gae-plugin', 'minikube', 'stackdriver', 'firebase']
 
-print('List of answers for the date range ' + str(start) + ' - ' + str(today))
+print('List of answers for the date range ' + str(start) + ' - ' + str(end))
 
 
 # Get all Stackoverflow answers for the date
 SITE = StackAPI('stackoverflow')
 answers = SITE.fetch('users/{ids}/answers', ids=so_user_list)
 for ans in answers['items']:
-    if (ans['creation_date']) > fromdate:
+    if (ans['creation_date']) > fromdate and (ans['creation_date']) <= todate:
         question_ids.append(ans['question_id'])
 
 # Creat a list of questions with supported tags-----
@@ -102,12 +117,13 @@ else:
 # Export to Sheets
 for ans in answers['items']:
     if ans['question_id'] in valid_q:
-        print("https://stackoverflow.com/questions/" + str(ans['answer_id']).lstrip()+" :"+ans['owner']['display_name']+str(ans['score']))
+        print("https://stackoverflow.com/questions/" + str(ans['answer_id']).lstrip()+": "+ans['owner']['display_name']+": "+str(ans['score'])+": "+str(datetime.fromtimestamp(ans['creation_date'])))
         # Insert results into spreadsheet
         wks.update_cell(row, 1, "https://stackoverflow.com/questions/" + str(ans['answer_id']))
         wks.update_cell(row, 2, ans['owner']['display_name'])
         wks.update_cell(row, 4, ans['is_accepted'])
         wks.update_cell(row, 5, ans['score'])
+        wks.update_cell(row, 6, str(datetime.fromtimestamp(ans['creation_date'])))
         if ans['owner']['user_id'] in so_k8s_users:
             cnt_k8s = cnt_k8s + 1
             wks.update_cell(row, 3, 'K8s')
@@ -129,7 +145,7 @@ valid_q = []
 SITE = StackAPI('serverfault')
 answers = SITE.fetch('users/{ids}/answers', ids=sf_user_list)
 for ans in answers['items']:
-    if (ans['creation_date']) > fromdate:
+    if (ans['creation_date']) > fromdate and (ans['creation_date']) <= todate:
         question_ids.append(ans['question_id'])
 
 # Creat a list of questions with supported tags-----
@@ -150,6 +166,7 @@ for ans in answers['items']:
         wks.update_cell(row, 2, ans['owner']['display_name'])
         wks.update_cell(row, 4, ans['is_accepted'])
         wks.update_cell(row, 5, ans['score'])
+        wks.update_cell(row, 6, str(datetime.fromtimestamp(ans['creation_date'])))
         if ans['owner']['user_id'] in sf_k8s_users:
             cnt_k8s = cnt_k8s + 1
             wks.update_cell(row, 3, 'K8s')
@@ -162,7 +179,8 @@ for ans in answers['items']:
         elif ans['owner']['user_id'] in sf_bigdata_users:
             cnt_bigdata = cnt_bigdata + 1
             wks.update_cell(row, 3, 'Bigdata')
-    row = row + 1
+        row = row + 1
+
 print("K8s:", cnt_k8s)
 print("Platform:", cnt_platform)
 print("Infra:", cnt_infra)
